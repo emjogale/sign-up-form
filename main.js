00be5d891747b8b029a9a34c0ce6d95f896885b5
Emma Gale <emmajgale@hotmail.com>
2021-08-02 20:30:16 +0100

    adding in the input checking loop

--- main.js
+++ main.js
@@ -6,14 +6,21 @@ const secondName = document.getElementById("second-name");
 const password = document.getElementById("password");
 const form = document.querySelector("form");
 console.log(form);
+const inputs = document.querySelectorAll("input");
+console.log(inputs);
 
 // define the function to check if any of these fields are empty
 const checkInput = (e) => {
 	e.preventDefault();
 	console.log("submit action fired");
-	return firstName.value === ""
-		? console.log("First Name cannot be empty")
-		: console.log("thanks");
+	inputs.forEach((input) => {
+		if (input.value === "") {
+			console.log(`the input is empty`);
+		}
+	});
+	// return firstName.value === ""
+	// 	? console.log("First Name cannot be empty")
+	// 	: console.log("thanks");
 };
 
 // add the function to the submit event
