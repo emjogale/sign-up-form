3941a21c50fcedd09dc41c6d048312cf08d381d1
Emma Gale <emmajgale@hotmail.com>
2021-08-05 17:58:32 +0100

    addi in function to validate email

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
 // define all the variables to use
 const firstName = document.getElementById("First-Name");
-console.log(firstName);
-console.log(firstName.value === "");
 const secondName = document.getElementById("Last-Name");
 const password = document.getElementById("Password");
 const form = document.querySelector("form");
-console.log(form);
 const inputs = document.querySelectorAll("input");
 console.log(inputs);
+
+// define a function to check if email is in valid format
+const isValid = function (email) {
+	return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+};
 
 // define the function to check if any of these fields are empty
 const checkInput = (e) => {
 	e.preventDefault();
-	console.log("submit action fired");
 	inputs.forEach((input) => {
-		if (input.id === "Password") {
-			console.log("this is the password field");
-		} else if (input.value === "") {
+		if (input.value === "") {
 			console.log(`${input.id} cannot be empty`);
 		}
 	});
