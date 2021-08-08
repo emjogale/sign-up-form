e5c3fbe12b69449ff9c9775c64d6753d582d7713
Emma Gale <emmajgale@hotmail.com>
2021-08-08 17:49:35 +0100

    adding in validation to password and firstname

--- app.js
+++ app.js
@@ -2,12 +2,20 @@
 const firstNameEl = document.querySelector("#first-name");
 const lastNameEl = document.querySelector("#last-name");
 const passwordEl = document.querySelector("#password");
+const emailEl = document.querySelector("#email");
 const form = document.querySelector("form");
+
 console.log(form);
+// select the input values
+
+const lastName = lastNameEl.value.trim();
+const password = passwordEl.value.trim();
 
 // reusable utility functions
 // check if a field has been filled in
-const isFilledIn = (value) => (value = "" ? false : true);
+const isFilledIn = (value) => (value === "" ? false : true);
+const word = "yes";
+console.log(isFilledIn(word));
 
 // check if an email is valid
 const isEmailValid = (email) => {
@@ -30,8 +38,42 @@ const showError = (input, message) => {
 const success = (input) => {
 	// get the parent of the input field
 	const formField = input.parentElement; //remove the error class
-	formField.remove("error");
+
+	formField.classList.remove("error");
 	// hide the error message
 	const error = formField.querySelector("small");
 	error.textContent = "";
 };
+
+// validating the name and password fields have been filled in
+const checkfirstName = () => {
+	let valid = false;
+	const firstName = firstNameEl.value.trim();
+	console.log(firstName);
+	if (!isFilledIn(firstName)) {
+		showError(firstNameEl, "First Name cannot be empty");
+	} else {
+		success(firstNameEl);
+		valid = true;
+	}
+};
+
+const checkEmail = () => {
+	let valid = false;
+	const email = emailEl.value.trim();
+	if (!isEmailValid(email)) {
+		showError(emailEl, "Looks like this is not an email");
+	} else {
+		success(emailEl);
+		valid = true;
+	}
+};
+
+// attache the submit event listener to the form
+form.addEventListener("submit", function (e) {
+	// prevent the form from submitting
+	e.preventDefault();
+	// validate the form
+	checkEmail();
+	checkfirstName();
+});
