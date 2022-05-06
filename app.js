26cfb5c49d1cb18dcb0747d17b20da9dcba95631
Emma Gale <emmajgale@hotmail.com>
2022-05-06 10:53:51 +0100

    tidy up desktop styles

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ const isFilledIn = (value) => (value === "" ? false : true);
 
 // check if an email is valid
 const isEmailValid = (email) => {
-	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	const re =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(email);
 };
 
@@ -88,9 +89,8 @@ const checkPassword = () => {
 	}
 };
 
-// attache the submit event listener to the form
+// attach the submit event listener to the form
 form.addEventListener("submit", function (e) {
-	console.log("trying to validate form");
 	// prevent the form from submitting
 	e.preventDefault();
 	// validate the form
